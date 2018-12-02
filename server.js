5a0f539ebc84884d3bca970734005b313c7388f2
Sharad <sharad143in@gmail.com>
2018-12-02 18:44:41 +0530

    updatedcode

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('*', function(req,res){
     res.sendFile(path.resolve(__dirname,'public','index.html'));
 })
 
-app.listen(3004, function(){
-    console.log("app is listening on port 3004");
+app.listen(3014, function(){
+    console.log("app is listening on port 3014");
 })
